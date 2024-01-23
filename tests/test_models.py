297141f6123c99c168dcf17ae903f3f0d288a77a
Lucas Citadini <l.ian.citadini@gmail.com>
2024-01-23 10:35:02 -0500

    added some test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -27,7 +27,7 @@ import os
 import logging
 import unittest
 from decimal import Decimal
-from service.models import Product, Category, db, DataValidationError
+from service.models import Product, db, DataValidationError
 from service import app
 from tests.factories import ProductFactory
 
@@ -182,3 +182,28 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.category, category)
+
+    def test_deserialize_with_invalid_available_value(self):
+        """It should return DataValidationError on deserialize with invalid available value"""
+        product = ProductFactory()
+        product.available = "Invalid"
+        data = product.serialize()
+        self.assertRaises(DataValidationError, product.deserialize, data)
+
+    def test_deserialize_with_no_data(self):
+        """It should return DataValidationError on deserialize with no data"""
+        product = Product()
+        data = None
+        self.assertRaises(DataValidationError, product.deserialize, data)
+
+    def test_find_by_price(self):
+        """It should Find Products by Price"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(price)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.price, price)
